3aedae6ed84fae6db6ca1f39aa4e303f6e7bf45d
jotabraham <jotabraham@gmail.com>
2021-10-15 13:37:44 -0400

    add btn func on shopping edit comp

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Pasta', 'Noodles and sOWce', 'https://images.squarespace-cdn.com/content/v1/55db39ece4b06065680e83b3/1627316913642-W1H3SU2HNI1NHNZDFMAI/IMG_1787-ETPhoto.jpg?format=1500w')
+    new Recipe('Pasta', 'Noodles and sOWce', 'https://images.squarespace-cdn.com/content/v1/55db39ece4b06065680e83b3/1627316913642-W1H3SU2HNI1NHNZDFMAI/IMG_1787-ETPhoto.jpg?format=1500w'),
+    new Recipe('A Test Recipe', 'this is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  };
 
 }
